4eeabc566847b9fdfb84835f9c4cd411775aa3ea
Charlemagne Wong <frian13@hotmail.com>
2020-09-22 20:46:35 -0500

    Changed db schema for bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_225407) do
+ActiveRecord::Schema.define(version: 2020_09_23_003824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2020_09_22_225407) do
     t.string "last_name"
     t.string "email"
     t.string "username"
-    t.string "password"
     t.boolean "is_admin"
     t.integer "points"
     t.integer "events", array: true
@@ -47,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_225407) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
     t.string "provider"
+    t.string "password_digest"
   end
 
 end
