ecafc1f5a722cdc5a9525e143d3ddc6f5baf0db1
isabelmcclure <isabelmcclure@tamu.edu>
2020-09-25 12:00:37 -0500

    changed redirect after signin and added logout

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -3,7 +3,7 @@ class RegisterController < ApplicationController
   def show
     # if user is signed in, redirect to page about user
     if user_signed_in?
-      redirect_to(me_path)
+      redirect_to(dashboard_path)
     end
 
     @user ||= User.new
